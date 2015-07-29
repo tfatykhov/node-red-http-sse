299bf9b9d5a7c3cb5c7eeda08cc86212240ba737
Olav Frengstad <olav@fwt.no>
2015-07-29 20:33:29 +0200

    init

---
+++ http-sse.js
@@ -0,0 +1,44 @@
+
+module.exports = function(RED) {
+	"use strict";
+	var EventSource = require('eventsource')
+
+	function SSENode(config) {
+		var node = this
+
+		RED.nodes.createNode(node, config);
+
+		this.remote = config.remote
+		this.events = (config.events || "message").split(/,/)
+
+		this.status({fill: 'red', shape: 'ring', text: 'common.status.disconnected'})
+
+		if (this.remote) {
+			this.client = new EventSource(this.remote)
+
+			this.client.onopen = function() {
+				node.status({fill: "green", shape: "dot", text: "common.status.connected"})
+			}
+
+			for (var i in this.events)
+				this.client.addEventListener(this.events[i], function(e) {
+					node.send({
+						event: e.type,
+						payload: e.data
+					})
+				})
+
+			this.client.onerror = function(err) {
+				node.status({fill: "red", shape: "dot", text: "common.status.error"});
+			}
+		}
+
+		this.on("close", function() {
+			node.status({fill: "red", shape: "ring", text: "common.status.disconnected"});
+			if (this.client)
+				this.client.close()
+		});
+	}
+
+	RED.nodes.registerType("http-sse", SSENode);
+}
