c697822060a0cacba19e7512ba2c3ffd71cc773a
Timur Fatykhov <tfatykhov@gmail.com>
2016-05-08 11:54:00 -0400

    Update http-sse.js

--- http-sse.js
+++ http-sse.js
@@ -7,37 +7,39 @@ module.exports = function(RED) {
 		var node = this
 
 		RED.nodes.createNode(node, config);
+		this.on('input', msg => {
+			this.status({fill: 'yellow', shape: 'dot', text: 'Requesting'});
+            this.remote = msg.remote
+            this.events = (msg.events || "message").split(/,/)
 
-		this.remote = config.remote
-		this.events = (config.events || "message").split(/,/)
+            this.status({fill: 'red', shape: 'ring', text: 'common.status.disconnected'})
 
-		this.status({fill: 'red', shape: 'ring', text: 'common.status.disconnected'})
+            if (this.remote) {
+                this.client = new EventSource(this.remote)
 
-		if (this.remote) {
-			this.client = new EventSource(this.remote)
+                this.client.onopen = function() {
+                    node.status({fill: "green", shape: "dot", text: "common.status.connected"})
+                }
 
-			this.client.onopen = function() {
-				node.status({fill: "green", shape: "dot", text: "common.status.connected"})
-			}
+                for (var i in this.events)
+                    this.client.addEventListener(this.events[i], function(e) {
+                        node.send({
+                            event: e.type,
+                            payload: e.data
+                        })
+                    })
 
-			for (var i in this.events)
-				this.client.addEventListener(this.events[i], function(e) {
-					node.send({
-						event: e.type,
-						payload: e.data
-					})
-				})
+                this.client.onerror = function(err) {
+                    node.status({fill: "red", shape: "dot", text: "common.status.error"});
+                }
+            }
 
-			this.client.onerror = function(err) {
-				node.status({fill: "red", shape: "dot", text: "common.status.error"});
-			}
-		}
-
-		this.on("close", function() {
-			node.status({fill: "red", shape: "ring", text: "common.status.disconnected"});
-			if (this.client)
-				this.client.close()
-		});
+            this.on("close", function() {
+                node.status({fill: "red", shape: "ring", text: "common.status.disconnected"});
+                if (this.client)
+                    this.client.close()
+            });
+    	});    
 	}
 
 	RED.nodes.registerType("http-sse", SSENode);
